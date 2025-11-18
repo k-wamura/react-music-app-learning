942351092e32c9f905d12a66dd4d775e252f96c3
kazu <kazuwamu0923@gmail.com>
2025-11-19 06:28:59 +0900

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>音楽生成アプリ</h1>
+      <Button>生成する</Button>
+    </div>
+  )
+}
+
+export default App
